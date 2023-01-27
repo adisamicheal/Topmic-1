a567b466e871989f4975593c1e8a9a9300fdd8c6
Micheal Adisa <adisamicheal20@gmail.com>
2023-01-27 18:54:31 +0000

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ var PurifyCSSPlugin= require('purifycss-webpack');
 
 module.exports = {
   entry: [
-    'babel-polyfill'
+    'babel-polyfill',
     './src/js/index.jsx',
     './src/css/styles.scss'
   ],
